c15769832c4fb8c4fa39f63b7f75cceb19634c10
KuznetsLex <kuznets.lex@gmail.com>
2024-03-07 20:29:53 +0600

    add bellman-ford algorithm and one test to it

--- src/test/java/org/shortest_path_problem/omsu/AlgorithmsTest.java
+++ src/test/java/org/shortest_path_problem/omsu/AlgorithmsTest.java
@@ -1,6 +1,7 @@
 package org.shortest_path_problem.omsu;
 
 import org.junit.jupiter.api.Test;
+import org.shortest_path_problem.omsu.data_types.PointersAndDistances;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,7 +14,15 @@ class AlgorithmsTest {
 
     @Test
     void bellmanFord() {
-        // TODO
+        double[][] graphWeightMatrix = {
+                {Double.POSITIVE_INFINITY, 4, 2},
+                {Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY},
+                {Double.POSITIVE_INFINITY, 1, Double.POSITIVE_INFINITY}
+        };
+        int[] p = {0, 2, 0};
+        double[] d = {0, 3, 2};
+
+        assertEquals(new PointersAndDistances(p, d), Algorithms.bellmanFord(0, graphWeightMatrix));
     }
 
     @Test
