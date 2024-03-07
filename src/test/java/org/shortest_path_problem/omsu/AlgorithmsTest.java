c7c4c78aa031fba8951a32e434d9c05cebdba90b
RuslanShaikin <ruslan.shayking@gmail.com>
2024-03-07 21:05:57 +0600

    added 1 dijkstra test

--- src/test/java/org/shortest_path_problem/omsu/AlgorithmsTest.java
+++ src/test/java/org/shortest_path_problem/omsu/AlgorithmsTest.java
@@ -3,29 +3,18 @@ package org.shortest_path_problem.omsu;
 import org.junit.jupiter.api.Test;
 import org.shortest_path_problem.omsu.data_types.PointersAndDistances;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class AlgorithmsTest {
 
     @Test
-    void dijkstra() {
-        double[][] graph = new double[4][4];
-        graph[0][0] = Double.POSITIVE_INFINITY;
-        graph[1][1] = Double.POSITIVE_INFINITY;
-        graph[2][2] = Double.POSITIVE_INFINITY;
-        graph[3][3] = Double.POSITIVE_INFINITY;
-        graph[0][1] = 0;
-        graph[0][2] = 1;
-        graph[0][3] = 2;
-        graph[1][0] = 0;
-        graph[1][2] = 2;
-        graph[1][3] = 0;
-        graph[2][0] = 1;
-        graph[2][1] = 2;
-        graph[2][3] = 1;
-        graph[3][0] = 2;
-        graph[3][1] = 0;
-        graph[3][2] = 1;
+    void dijkstraFirst() {
+        double[][] graph = {
+                {Double.POSITIVE_INFINITY, 0, 1, 2},
+                {0, Double.POSITIVE_INFINITY, 2, 0},
+                {1, 2, Double.POSITIVE_INFINITY, 1},
+                {2, 0, 1, Double.POSITIVE_INFINITY}
+        };
 
         int[] pointers = {1, 3, 3, 3};
         double[] distances = {0, 0, 1, 0};
