3dbef5369087a9193352569da3684c2dff3c14d3
RuslanShaikin <ruslan.shayking@gmail.com>
2024-03-07 19:08:15 +0600

    added dijkstra test

--- src/test/java/org/shortest_path_problem/omsu/AlgorithmsTest.java
+++ src/test/java/org/shortest_path_problem/omsu/AlgorithmsTest.java
@@ -1,14 +1,36 @@
 package org.shortest_path_problem.omsu;
 
 import org.junit.jupiter.api.Test;
+import org.shortest_path_problem.omsu.data_types.PointersAndDistances;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AlgorithmsTest {
 
     @Test
     void dijkstra() {
-        // TODO
+        double[][] graph = new double[4][4];
+        graph[0][0] = Double.POSITIVE_INFINITY;
+        graph[1][1] = Double.POSITIVE_INFINITY;
+        graph[2][2] = Double.POSITIVE_INFINITY;
+        graph[3][3] = Double.POSITIVE_INFINITY;
+        graph[0][1] = 0;
+        graph[0][2] = 1;
+        graph[0][3] = 2;
+        graph[1][0] = 0;
+        graph[1][2] = 2;
+        graph[1][3] = 0;
+        graph[2][0] = 1;
+        graph[2][1] = 2;
+        graph[2][3] = 1;
+        graph[3][0] = 2;
+        graph[3][1] = 0;
+        graph[3][2] = 1;
+
+        int[] pointers = {1, 3, 3, 3};
+        double[] distances = {0, 0, 1, 0};
+
+        assertEquals(new PointersAndDistances(pointers, distances), Algorithms.dijkstra(3, graph));
     }
 
     @Test
