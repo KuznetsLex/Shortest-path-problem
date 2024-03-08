204174511b227d6d87320f9eec6adee75d625d44
Kate <katya700700@gmail.com>
2024-03-08 20:34:59 +0600

    added PathForDijkstra

--- src/main/java/org/shortest_path_problem/omsu/Path.java
+++ src/main/java/org/shortest_path_problem/omsu/Path.java
@@ -1,27 +1,26 @@
 package org.shortest_path_problem.omsu;
 
 import org.shortest_path_problem.omsu.data_types.PathAndDistance;
-import org.shortest_path_problem.omsu.data_types.PointersAndDistancesMatrices;
+import org.shortest_path_problem.omsu.data_types.PointersAndDistances;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
+import static org.shortest_path_problem.omsu.Algorithms.dijkstra;
+
 public class Path {
-
-    public static PathAndDistance pathForFloydWarshall(int s, int t, double[][] graphWeightMatrix) {
-        PointersAndDistancesMatrices matrices = Algorithms.floydWarshall(graphWeightMatrix);
-        List<Integer> path = new ArrayList<>(graphWeightMatrix[0].length);
-        double distance = matrices.getDistances()[s][t];
-
-        path.add(t);
-
-        while(s != t) {
-            t = matrices.getPointers()[s][t];
-            path.add(t);
+    public static PathAndDistance pathForDijkstra(int s, int t, double[][] graphWeightMatrix) {
+        List<Integer> pathList = new ArrayList<>();
+        int[] p = Algorithms.dijkstra(s, graphWeightMatrix).getPointers();
+        double[] d = Algorithms.dijkstra(s, graphWeightMatrix).getDistances();
+        int v = t;
+        pathList.add(v);
+        while(v!=s) {
+            int u = p[v];
+            pathList.addFirst(u);
+            v = u;
         }
-        Collections.reverse(path);
-
-        return new PathAndDistance(path, distance);
+        double distance = d[t];
+        return new PathAndDistance(pathList, distance);
     }
 }
