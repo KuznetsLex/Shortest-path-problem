dfb6a05c14303b24af8155349a773f8d8c18fab3
konovalovme2 <konovalovme2@gmail.com>
2024-03-09 21:54:11 +0600

    Добавлен ввод случайного графа

--- src/main/java/org/shortest_path_problem/omsu/GraphInput.java
+++ src/main/java/org/shortest_path_problem/omsu/GraphInput.java
@@ -10,10 +10,10 @@ public class GraphInput {
     public static double[][] randomInput(int size) {
         double[][] graph = new double[size][size];
         Random rand = new Random();
+        double probability = 0.15;
 
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                double probability = 0.15;
                 double realEvent = rand.nextDouble();
                 if(i==j){
                     graph[i][j] = 0;
