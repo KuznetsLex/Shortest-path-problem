9b0fbb87aad81f42c4533eeb33ac4cb15792ac1f
RuslanShaikin <ruslan.shayking@gmail.com>
2024-03-09 16:07:28 +0600

    added path for FloydWarshall

--- src/main/java/org/shortest_path_problem/omsu/Path.java
+++ src/main/java/org/shortest_path_problem/omsu/Path.java
@@ -1,10 +1,27 @@
 package org.shortest_path_problem.omsu;
 
 import org.shortest_path_problem.omsu.data_types.PathAndDistance;
+import org.shortest_path_problem.omsu.data_types.PointersAndDistancesMatrices;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Path {
-    public static PathAndDistance path(int s, int t, double[][] graphWeightMatrix) {
-        // TODO
-        return null;
+
+    public static PathAndDistance PathForFloydWarshall(int s, int t, double[][] graphWeightMatrix) {
+        PointersAndDistancesMatrices matrices = Algorithms.floydWarshall(graphWeightMatrix);
+        List<Integer> path = new ArrayList<>(graphWeightMatrix[0].length);
+        double distance = matrices.getDistances()[s][t];
+
+        path.add(t);
+
+        while(s != t) {
+            t = matrices.getPointers()[s][t];
+            path.add(t);
+        }
+        Collections.reverse(path);
+
+        return new PathAndDistance(path, distance);
     }
 }
