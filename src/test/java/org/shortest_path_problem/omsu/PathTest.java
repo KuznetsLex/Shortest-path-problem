83ed58a26ae7e88649ce479d31087887a754db31
RuslanShaikin <ruslan.shayking@gmail.com>
2024-03-09 15:33:25 +0600

    Added pathForDijkstra Test (needs to be fixed)

--- src/test/java/org/shortest_path_problem/omsu/PathTest.java
+++ src/test/java/org/shortest_path_problem/omsu/PathTest.java
@@ -1,12 +1,28 @@
 package org.shortest_path_problem.omsu;
 
 import org.junit.jupiter.api.Test;
+import org.shortest_path_problem.omsu.data_types.PathAndDistance;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.shortest_path_problem.omsu.Path.path;
 
 class PathTest {
 
     @Test
-    void pathForDijkstra() {
-        // TODO
+    void pathForDijkstraFirst() {
+        double[][] graph = {
+                {Double.POSITIVE_INFINITY, 0, 1, 2},
+                {0, Double.POSITIVE_INFINITY, 2, 0},
+                {1, 2, Double.POSITIVE_INFINITY, 1},
+                {2, 0, 1, Double.POSITIVE_INFINITY}
+        };
+
+        List<Integer> path = new ArrayList<>(Arrays.asList(3, 1, 0));
+        assertEquals(new PathAndDistance(path, 0), path(3, 0, graph));
     }
 
     @Test
