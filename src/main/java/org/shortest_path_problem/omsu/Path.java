db788072fc3ac865c4d0b73134b227b4b80d3730
Kate <katya700700@gmail.com>
2024-03-10 15:15:28 +0600

    deleted PathForDijkstra

--- src/main/java/org/shortest_path_problem/omsu/Path.java
+++ src/main/java/org/shortest_path_problem/omsu/Path.java
@@ -11,19 +11,7 @@ import static org.shortest_path_problem.omsu.Algorithms.bellmanFord;
 import static org.shortest_path_problem.omsu.Algorithms.dijkstra;
 
 public class Path {
-    public static PathAndDistance pathForDijkstra(int s, int t, double[][] graphWeightMatrix) {
-        List<Integer> pathList = new ArrayList<>();
-        PointersAndDistances pointersAndDistances = new PointersAndDistances(dijkstra(s, graphWeightMatrix));
-        int v = t;
-        pathList.add(v);
-        while(v!=s) {
-            int u = pointersAndDistances.getPointers()[v];
-            pathList.addFirst(u);
-            v = u;
-        }
-        int distance = pointersAndDistances.getDistances()[t];
-        return new PathAndDistance(pathList, distance);
-    }
+
     public static PathAndDistance pathForBellmanFord(int s, int t, double[][] graphWeightMatrix) {
         List<Integer> pathList = new ArrayList<>(graphWeightMatrix[0].length);
         PointersAndDistances pointersAndDistances = bellmanFord(s, graphWeightMatrix);
