5366df2a9432fcf788d1bea71c69d9d2df95c41c
Kate <katya700700@gmail.com>
2024-03-10 12:29:14 +0600

    fixed pathForDijkstra according to Ruslans review

--- src/main/java/org/shortest_path_problem/omsu/Path.java
+++ src/main/java/org/shortest_path_problem/omsu/Path.java
@@ -4,37 +4,25 @@ import org.shortest_path_problem.omsu.data_types.PathAndDistance;
 import org.shortest_path_problem.omsu.data_types.PointersAndDistances;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-import static org.shortest_path_problem.omsu.Algorithms.bellmanFord;
 import static org.shortest_path_problem.omsu.Algorithms.dijkstra;
 
 public class Path {
     public static PathAndDistance pathForDijkstra(int s, int t, double[][] graphWeightMatrix) {
-        List<Integer> pathList = new ArrayList<>();
-        PointersAndDistances pointersAndDistances = new PointersAndDistances(dijkstra(s, graphWeightMatrix));
+        List<Integer> pathList = new ArrayList<>(graphWeightMatrix[0].length);
+        PointersAndDistances pointersAndDistances = dijkstra(s, graphWeightMatrix);
         int v = t;
         pathList.add(v);
         while(v!=s) {
             int u = pointersAndDistances.getPointers()[v];
-            pathList.addFirst(u);
+            pathList.add(u);
             v = u;
         }
+        Collections.reverse(pathList);
         double distance = pointersAndDistances.getDistances()[t];
         return new PathAndDistance(pathList, distance);
     }
 
-    public static PathAndDistance pathForBellmanFord(int s, int t, double[][] graphWeightMatrix) {
-        List<Integer> pathList = new ArrayList<>();
-        PointersAndDistances pointersAndDistances = new PointersAndDistances(bellmanFord(s, graphWeightMatrix));
-        int v = t;
-        pathList.add(v);
-        while(v!=s) {
-            int u = pointersAndDistances.getPointers()[v];
-            pathList.addFirst(u);
-            v = u;
-        }
-        int distance = pointersAndDistances.getDistances()[t];
-        return new PathAndDistance(pathList, distance);
-    }
 }
